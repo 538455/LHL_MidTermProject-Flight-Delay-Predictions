a6a3c533456d309fc5df9c1176aa179024247411
Sebastien <sebastien.garneau@gmail.com>
2022-11-26 11:35:56 -0500

    Uploading DataCleaning_Template and EDA

---
+++ src/modules/pgAdmin_EDA.sql
@@ -0,0 +1,43 @@
+-- Let's get an overview of the tables in the database
+-- -- Table: flight_subset
+-- SELECT * FROM flight_subset LIMIT 100
+-- This table contains an avg delay (departure and arrival) for each carrier, per year/month/route. Route is depicted using the airport ID of the departing airport and the arrival airport
+
+-- -- Table: flights_temp
+-- SELECT * FROM flights_temp LIMIT 100
+---- Same number of rows as flights, but 5x additional columns
+
+-- -- Table: pass_sample
+-- SELECT * FROM pass_sample LIMIT 100
+----- Seems to be a monthly consolidation of how many times in a month an airline did a route, to include total number of passenger.
+------OF NOTE: There's a distance column for each route which could be useful for calculating fuel consumption
+
+-- -- Table: passenger_subset
+-- SELECT * FROM passenger_subset LIMIT 100
+
+-- -- Table: passengers_aggregated
+-- SELECT * FROM passengers_aggregated LIMIT 100
+
+-- -- Table: sample
+-- SELECT * FROM sample LIMIT 100
+
+-- -- Table: temptable
+-- SELECT * FROM temptable LIMIT 100
+
+-- -- Table: test_table
+-- SELECT * FROM test_table LIMIT 100
+
+-- -- Table: week_one
+-- SELECT * FROM week_one LIMIT 100
+--------------------------------------------------------------------------------------------
+-- DESCRIPTION OF TABLES BELOW ARE PROVIDED ALREADY
+-- -- Table: flights
+-- SELECT * FROM flights LIMIT 100
+
+-- -- Table: passengers
+-- SELECT * FROM passengers LIMIT 100
+
+-- -- Table: fuel_comsumption
+-- SELECT * FROM fuel_comsumption LIMIT 100
+-- -- Table: flights_test
+-- SELECT * FROM flights_test LIMIT 100
